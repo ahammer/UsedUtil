d206d41967b71443f0030e4b5acdd674bd64c6c3
adamhammer2@gmail.com <Iluv5u5h1>
2016-06-14 23:41:16 -0700

    Initial import

---
+++ src/main/java/com/metalrain/UsedUtilDelegate.java
@@ -0,0 +1,123 @@
+package com.metalrain;
+
+import java.util.*;
+
+/**
+ * This is the actual implementation, in a non-static way
+ *
+ * This API is not meant to be user facing
+ */
+public class UsedUtilDelegate {
+    Map<String, Integer> callCountMap = new HashMap<String, Integer>();
+
+    void allowOnce(String tag, StackTraceElement[] stackTrace) {
+        String key = CallKeyGenerator.fromStackTrace(tag, stackTrace);
+        incrementCallCountMap(key);
+        if (callCountMap.get(key) > 1) throw new RuntimeException("You've specified that you call this only once");
+    }
+
+    void logCall(String tag, StackTraceElement[] stackTrace) {
+        String key = CallKeyGenerator.fromStackTrace(tag, stackTrace);
+        incrementCallCountMap(key);
+    }
+
+    boolean isUsed(String tag) {
+        if (tag == null) throw new IllegalArgumentException("Tag must not be null");
+        for (String key:callCountMap.keySet()) {
+            if ((key.startsWith(":") && tag.equals("")) || key.startsWith(tag))
+                return false;
+        }
+        return true;
+    }
+
+    String getReport() {
+        Set<String> tags = getTags();
+        String report = "UsedUtil Runtime Report\n";
+        if (tags.size() > 0) {
+            report += "Tags Detected: "+tags.toString() +"\n";
+        } else {
+            report += "No Tags Detected";
+        }
+
+        report +="\nUntagged Calls";
+        for (String key:callCountMap.keySet()) {
+            if (key.startsWith(":")) {
+                report += "\n\tKey = "+key.substring(1);
+                report += "\n\tCount = "+callCountMap.get(key)+"\n";
+            }
+        }
+
+        for (String tag:tags) {
+            report +="\nTagged Calls: "+tag;
+            for (String key:callCountMap.keySet()) {
+                if (key.startsWith(tag)) {
+                    report += "\n\tKey = "+key.substring(tag.length()+1);
+                    report += "\n\tCount = "+callCountMap.get(key)+"\n";
+                }
+            }
+
+        }
+        return report;
+    }
+
+
+    /**
+     * Increments the count for a key
+     * @param key, the key based on the stacktrace
+     */
+    private void incrementCallCountMap(String key) {
+        Integer count = callCountMap.get(key);
+        if (count == null)
+            count = 1;
+        else
+            count++;
+        callCountMap.put(key, count);
+    }
+
+
+    /**
+     * Iterates over the keys and grabs the tags
+     * @return
+     */
+    private Set<String> getTags() {
+        Set<String> keys = callCountMap.keySet();
+        Set<String> tags = new HashSet<String>();
+        for (String key:keys) {
+            if (!key.startsWith(":") && key.contains(":")) {
+                String tag = key.split(":")[0];
+                if (!tags.contains(tag))tags.add(tag);
+            }
+        }
+        return Collections.unmodifiableSet(tags);
+    }
+
+    /**
+     * Counts all the usages of a tag
+     *
+     * @param myTag
+     * @return
+     */
+    public int getUsageCount(String myTag) {
+        int count = 0;
+        for (String tag:getTags()) {
+            if (tag.equals(myTag)) {
+                for (String key:callCountMap.keySet()) {
+                    if (key.split(":")[0].equals(tag)) count+=callCountMap.get(key);
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Get's the total usage count
+     * @return
+     */
+    public int getUsageCount() {
+        int count = 0;
+        for (String key:callCountMap.keySet()) {
+            count+=callCountMap.get(key);
+        }
+        return count;
+    }
+}
---
+++ src/test/java/com/metalrain/UsedUtilTest.java
@@ -0,0 +1,101 @@
+package com.metalrain;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.metalrain.UsedUtil.*;
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test Cases to Demonstrate simple usage of the tool
+ */
+public class UsedUtilTest {
+
+    class MyTestClass {
+        public MyTestClass() {}
+
+        public void doSomething() {
+            log();
+        }
+
+        public void doSomethingTagged() {
+            log("myTag");
+        }
+        public void doSomethingElseTagged() {
+            log("myTag2");
+        }
+
+        public void initialize() {
+            allowOnce();
+        }
+    }
+
+    @Before
+    public void setup() {
+        UsedUtil.reset();
+    }
+
+    @After
+    public void debugOutput() {
+        UsedUtil.printReport();
+    }
+
+    /**
+     * We are testing that calls to Log() and Log(tag) properly reflect in calls to isUsed()
+     */
+    @Test
+    public void testIsDead() throws Exception {
+        MyTestClass m = new MyTestClass();
+
+        //Verify the untagged state
+        assertTrue(isUsed());
+        m.doSomething();
+        assertFalse(isUsed());
+
+        //Verify a tagged state
+        assertTrue(isUsed("myTag"));
+        m.doSomethingTagged();
+        assertFalse(isUsed("myTag"));
+    }
+
+    /**
+     * Verify that call once functionality throws a exception when installed
+     */
+    @Test(expected = RuntimeException.class)
+    public void testAllowCallOnceThrows() {
+        MyTestClass m = new MyTestClass();
+        m.initialize();
+        m.initialize();
+    }
+
+    /**
+     * Verify that CallOnce doesn't cause a problem when you call it once
+     */
+    @Test
+    public void testAllowCallOnceValid() {
+        MyTestClass m = new MyTestClass();
+        m.initialize();
+        assertEquals(UsedUtil.getUsageCount(), 1);
+    }
+
+    /**
+     * Count usage per tag, or overall
+     */
+    @Test
+    public void testCount() {
+        MyTestClass m = new MyTestClass();
+        m.doSomethingTagged();
+        m.doSomethingTagged();
+        m.doSomethingTagged();
+        m.doSomethingElseTagged();
+        m.doSomethingElseTagged();
+        assertEquals(UsedUtil.getUsageCount("myTag"), 3);
+        assertEquals(UsedUtil.getUsageCount("myTag2"), 2);
+        assertEquals(UsedUtil.getUsageCount(), 5);
+    }
+
+
+}
